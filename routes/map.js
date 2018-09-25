2d003b57c7de5d458c17725129ea1564b4360051
dtryan24 <dtryan24@gmail.com>
2018-09-25 16:35:20 -0500

    Change parsing of Zillow data

--- routes/map.js
+++ routes/map.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var ZillowFile = require('../views/zillowCall');
+var ZillowFile = require('./zillowCall');
 var Zillow = require("node-zillow");
 
 const apiKey = require("../apiKey");
@@ -15,7 +15,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-	console.log("You sent %s and %s in the form", req.body.Max, req.body.Min);
+
+	console.log("Received %s and %s in the form", req.body.Max, req.body.Min);
 
 	var parameters = {
 		state: "TN",
@@ -25,50 +26,9 @@ router.post('/', function(req, res){
 
 	zillow.get("GetRegionChildren", parameters)
 		.then(function(results){
-			console.log("The results are: ");
+			console.log(ZillowFile.ZillowData(results));
 			res.render('map', {title: ZillowFile.ZillowData(results)});
 		})
-	/*
-	map.QuandlData(quandl.dataset("ZILLOW", "ZIP_RMP_37011", function(err, resp){
-		if (err){
-			throw err;
-		}
-
-		console.log(resp);
-		return resp;
-	}))
-	*/
 })
-
-
-/*
-var ZillowData = function zillowData(response){
-	var j = 0;
-
-	var numberOfLocations = parseInt(response.response.list.count);
-	var regionArray = response.response.list.region;
-
-	var neighborhoodsWithZIndex = [];
-
-	for (var i = 0; i < numberOfLocations; ++i){
-		var region = regionArray[i];
-
-		if (region.zindex != null){
-
-			var neighborhood = {};
-
-			neighborhood.ZIndex = region.zindex[0]._;
-			neighborhood.Lat = region.latitude[0];
-			neighborhood.Long = region.longitude[0];
-
-			neighborhoodsWithZIndex.push(neighborhood);
-
-			++j;
-		}
-	}
-
-	return neighborhoodsWithZIndex;
-}
-*/
 
 module.exports = router;

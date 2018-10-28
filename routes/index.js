cc87ab42b2d7ed32b5cb6e2af2f125daa25f69c4
thomasmallick <thomas.j.mallick@vanderbilt.edu>
2018-10-28 12:57:10 -0500

    Affordable fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nashville Affordable Housing Calculator' });
+  res.render('index', { title: 'Nashville Afforable Housing Calculator' });
 });
 
 
